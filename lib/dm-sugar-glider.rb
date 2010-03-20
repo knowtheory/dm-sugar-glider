a3939bb2cf3f4d49f265fad96a025ce228d500ca
Ted Han <ted.han@skein.us>
2010-03-20 19:08:00 -0400

    and should probably actually require the collection patching.

--- lib/dm-sugar-glider.rb
+++ lib/dm-sugar-glider.rb
@@ -6,16 +6,22 @@ dir = Pathname(__FILE__).dirname.expand_path + 'dm-sugar-glider'
 require 'dm-core'
 require 'dm-core/version'
 
+require dir / 'collection'
 require dir / 'model'
 require dir / 'model' / 'property'
 require dir / 'model' / 'query_block'
 require dir / 'query'
 require dir / 'query' / 'property_matcher'
 require dir / 'query' / 'relationship_matcher'
+#require dir / 'query' / 'conditions' / 'operand'
 require dir / 'relationship'
 require dir / 'support' / 'class_path'
 
 module DataMapper
+  class Collection
+#    include SugarGlider::Collection
+  end
+  
   class Query
     include SugarGlider::Query
   end
